fc244109cec71519cf89267df68462132d5ca2c3
Philipp Scheer <ph2378@hs-woe.de>
2024-06-14 08:49:23 +0200

    lightcardwip

--- src/app/dashboard/light-widget/light-widget.component.ts
+++ src/app/dashboard/light-widget/light-widget.component.ts
@@ -3,16 +3,23 @@ import { MatCardModule } from '@angular/material/card';
 import { MatGridListModule } from '@angular/material/grid-list';
 import {MatButtonModule} from '@angular/material/button';
 import {MatDividerModule} from '@angular/material/divider';
+import { ColorPickerModule } from 'ngx-color-picker';
 
 @Component({
   selector: 'app-light-widget',
   standalone: true,
   imports: [MatCardModule,
-    MatGridListModule, MatButtonModule, MatDividerModule],
+    MatGridListModule, MatButtonModule, MatDividerModule, ColorPickerModule],
   templateUrl: './light-widget.component.html',
   styleUrl: './light-widget.component.scss'
 })
 export class LightWidgetComponent {
+color: any;
+popup: string;
+togglelight() {
+console.log("toggle")
+
+}
 led1name: any;
 
 }
