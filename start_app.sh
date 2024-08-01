68bed64b2c888c318cf3e29dda91a9d037ef84c9
Philipp Scheer <philipp@die-scheers.de>
2024-08-01 15:05:39 +0200

    edited autostart

--- start_app.sh
+++ start_app.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 cd /home/pi/Dashboard/dist/dashboard/browser
 angular-http-server -p 9000
-sleep 10
-chromium-browser --kiosk localhost:9000
+chromium-browser --start-fullscreen localhost:9000
