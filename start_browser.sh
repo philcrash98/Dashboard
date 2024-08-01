ed9657e33e4abf1eaa7c2e88de81cc486a17deaf
philcrash <philipp@die-scheers.de>
2024-08-01 20:58:24 +0200

    changed startup script

--- start_browser.sh
+++ start_browser.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 sleep 10
-chromium-browser --start-fullscreen 127.0.0.1:9000
+export DISPLAY=:0
+export XAUTHORITY=/home/pi/.Xauthority
+export XDG_RUNTIME_DIR=/run/user/$(id -u)  # Hier die Umgebungsvariable setzen
+chromium-browser http://localhost:9000 --start-fullscreen --disable-gpu --disable-software-rasterizer 
